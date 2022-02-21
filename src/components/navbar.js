afd28694bd31cd83cd3de92bb59127188a89709c
Dians <alghazamumtaz@gmail.com>
2022-02-21 11:04:15 +0700

    progressif web app with react

---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+const navbar = () => {        
+
+    return (
+        <nav className="bar">
+            <ul className="menu">
+                <li>PRAKIRAAN CUACA</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default navbar;
